b9c9cfcdca87e70624941d03c6ce52730ce243d7
ayong <AyongNice@163.com>
2023-03-29 23:19:31 +0800

    main: 优化打包体积

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import {defineConfig} from 'vite'
-
+const config = require("./package.json")
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
@@ -7,6 +7,16 @@ export default defineConfig({
             entry: './index.ts',
             name: "muk-ui",
             fileName:'monito-action'
+        },
+        rollupOptions: {
+            output: {
+                // specify the output directory
+                dir: 'dist',
+                // specify the output file name
+                entryFileNames: `${config.name}.js`,
+                // specify the format of the output file
+                format: 'umd'
+            }
         }
     },
 })
